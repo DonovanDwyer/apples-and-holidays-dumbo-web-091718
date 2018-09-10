9be44b467119e1fe7b89dd3ccb18c2604d4c285c
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 23:54:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
       if day.to_s.include? "_"
         new_day = day.to_s.split("_")
         new_day.map {|x| x.capitalize!}
-        new_day.to_s
+        new_day = new_day.join(" ")
         puts "  #{new_day}: #{supplies.join(",")}"
       else
         puts "  #{day.to_s.capitalize}: #{supplies.join(", ")}"
