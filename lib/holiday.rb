911542112048395575da78da272fd1d88b87f174
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 23:41:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supplies|
+      if day.include?
       puts "  #{day.to_s.capitalize}: #{supplies.join(", ")}"
     end
   end
